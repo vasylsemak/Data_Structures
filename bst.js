403e3ca5afb2400966a4dc364b89c1dc8caec7b0
Vasyl Semak <vasylsmk1401@gmail.com>
2021-10-24 10:09:01 -0400

    add: insert() method to insert new node in BST

--- bst.js
+++ bst.js
@@ -9,10 +9,49 @@ class Node {
 class BST {
   constructor(val) {
     this.root = new Node(val)
-    this.count = 0
+    this.count = 1
   }
+
+  size() {
+    return this.count
+  }
+
+  // create new Node with val
+  // increment count
+  /*
+    if val is less than rootVal:
+      if no left - append left as new Node
+      else - recursively go in leftNode
+  */
+  // do same for right
+  insert(val) {
+    let newNode = new Node(val)
+    this.count++
+
+    const createNode = root => {
+      if(newNode.value < root.value) {
+        if(!root.left) root.left = newNode
+        else createNode(root.left)
+      }
+      if(newNode.value > root.value) {
+        if(!root.right) root.right = newNode
+        else createNode(root.right)
+      }
+      return newNode
+    }
+
+    createNode(this.root)
+  }
+
 }
 
-let myBst = new BST(55)
 
-console.log('myBst: ', myBst)
+let myBST = new BST(8)
+myBST.insert(4)
+myBST.insert(3)
+myBST.insert(6)
+myBST.insert(5)
+
+
+console.log('                                                       ')
+console.log('myBst: ', myBST.root)
